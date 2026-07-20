df761165a482c70edae75e94c57fcfd89ec06e79
sandhya <dellisandhya14@gmail.com>
2026-07-20 12:12:44 +0530

    day1

---
+++ main.py
@@ -0,0 +1,45 @@
+print('sandhya\ndelli')
+print('\tcurrent\new\floder')
+'''def even_odd():
+    num = int(input('Enter a number: '))
+    if num % 2 == 0:
+        print("even") 
+    else:
+        print("odd")
+even_odd()'''
+'''dict ={
+    "name":"akshaya",
+    "age" :18,
+    "course":"python"
+}
+dict["name"] = "sandhya"
+print(dict)
+print(dict.get("name"))
+print(dict.keys())
+print(dict.values())
+print(dict.items())'''
+'''def count(*args):
+    print(type(args))
+def count1(*kwargs):
+    print(type(kwargs))
+count(1,2,3,4,5)
+count1(name="sandhya", age=18, course="python")
+'''
+'''str="daiva sandhya"
+for i in range(1,len(str)-1):
+    print(str[i])
+    str[1:len(str)-1]
+    
+while true:
+    print("hello")
+    
+'''
+z=9
+s=0
+'''for i in range(1,z+1):
+    s+=i
+print(s)'''
+while z<0:
+    s+=z
+    z-=1  
+print(s)  
